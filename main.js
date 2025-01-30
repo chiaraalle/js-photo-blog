5ab52b7cb5d395b8a53a7ffbf142e38698c49e2c
chiaralle <chiaraallegrino@gmail.com>
2025-01-30 18:34:40 +0100

    add: cards

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 //`endpoint: https://lanciweb.github.io/demo/api/pictures/
-const contenitoreCard = document.querySelector(".container")
+const contenitoreCard = document.querySelector(".row")
 
 fetch("https://lanciweb.github.io/demo/api/pictures/")
 .then(response => response.json())
 .then(data => {
     console.log(data);
     data.forEach(element => {
-        contenitoreCard.innerHTML += ` 
-        <div class="row p-2">
-          <div class="card col-sm-12 col-md-6">
-            <img src="img/pin.svg" width="20px" class="pin-photo" alt="">
-            <img src=${element.url} class="card-img-top" alt="..."> 
-            <div class="card-body">
-              <h5 class="card-title">${element.title}</h5>
-              <p class="card-text">${element.date}</p>
-            </div>
+       contenitoreCard.innerHTML += `
+        <div class="col-sm-12 col-md-6 col-lg-4 position-relative mb-3">
+              <div class="card p-2">
+                <img class="position-absolute top-0 start-50 translate-middle" src="img/pin.svg" width="20px" class="pin-photo" alt="">
+                <img src="${element.url}" class="card-img-top" alt="..."> 
+                <div class="card-body">
+                  <h5 class="card-title">${element.title}</h5>
+                  <p class="card-text">${element.date}</p>
+                </div>
+              </div> 
           </div>
-        </div>`
+       `
     });
     
 })
